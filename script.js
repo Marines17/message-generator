4e49c8f558515ce5611e342cf323eddb8130c143
Marines17 <simon.marine33@gmail.com>
2022-11-10 20:14:57 +0000

    created array speed and color

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 let cars = ["BMW", "Audi", "Peugeot", "Mercedes", "Ford", "Honda", "Opel"];
+let color = ["blue", "grey", "orange", "black", "white", "red", "green"];
+let speed = [1, 2, 3, 4, 5];
+
+console.log(speed);
