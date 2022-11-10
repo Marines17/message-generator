ffbe6354cdb1bb7136448bcceb62315ef677d1d2
Marines17 <simon.marine33@gmail.com>
2022-11-10 20:27:13 +0000

    added random selections for arrays + console logged into a sentence

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let cars = ["BMW", "Audi", "Peugeot", "Mercedes", "Ford", "Honda", "Opel"];
-let color = ["blue", "grey", "orange", "black", "white", "red", "green"];
-let speed = [1, 2, 3, 4, 5];
+const cars = ["BMW", "Audi", "Peugeot", "Mercedes", "Ford", "Honda", "Opel"];
+const color = ["blue", "grey", "orange", "black", "white", "red", "green"];
+const speed = [1, 2, 3, 4, 5];
 
-console.log(speed);
+let randCar = cars[Math.floor(Math.random() * cars.length)];
+let randColor = color[Math.floor(Math.random() * color.length)];
+let randSpeed = speed[Math.floor(Math.random() * speed.length)];
+
+console.log(
+  `You have a ${randColor} ${randCar} and does 0 to 60mph in ${randSpeed} seconds !`
+);
